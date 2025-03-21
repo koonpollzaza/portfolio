e50eff57610fff89919874d2f7650ba0d304dabf
Tanapon <thanapon10187za@gmail.com>
2025-03-21 15:39:57 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <div className="flex justify-between items-center text-white">
             <img src={logo} className="App-logo w-20 rounded-full" alt="logo" />
             <ul className="hidden md:flex">
-            <li className="p-4"><a href="#home" className="hover:underline">About</a></li>
+            <li className="p-4"><a href="#home" className="hover:underline">About Me</a></li>
             <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
             <li className="p-4"><a href="#honors" className="hover:underline">Honor & Awards</a></li>
             <li className="p-4"><a href="#certs" className="hover:underline">Certfications</a></li>
